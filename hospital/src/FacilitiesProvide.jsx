18b55e8d86f4dc9ca282f308f08cec95bb25e291
Venom4K <adityakanojiya325@gmail.com>
2022-10-07 18:08:17 +0530

    change

--- hospital/src/FacilitiesProvide.jsx
+++ hospital/src/FacilitiesProvide.jsx
@@ -6,11 +6,15 @@ function FacilitiesProvide(){
         <h1 className="heading">What Facilities We Provide ? </h1>
         <div className="box">
         <div className="left-side">
-        <img src={require("./images/Doctor.png")} className="imgset"/>
+        <img src={require("./images/Doctor.png")} className="imgset img-fluid"/>
         </div>
-        <div className="right-side"></div>
+        <div className="right-side">
+        <div className="item">Item-1</div>
+        <div className="item">Item-2</div>
+        <div className="item">Item-3</div>
+        </div>
 
-
+ 
         </div>
          
         </div>
